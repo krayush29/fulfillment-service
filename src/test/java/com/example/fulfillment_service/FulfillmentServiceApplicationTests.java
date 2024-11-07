0150fd5abfd574335f3832e525a95b429c4766e4
kumar.ayush_ftc <kumar.ayush_ftc@external.swiggy.in>
2024-11-07 09:48:27 +0530

    Initiate fulfillment service

---
+++ src/test/java/com/example/fulfillment_service/FulfillmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fulfillment_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FulfillmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
